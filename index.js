4c8e7010493693cd847fcf5db65f4559a62956af
unknown <nguyenngoctuyen188@gmail.com>
2020-08-22 15:43:05 +0700

    change port

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const app = express()
-const port = 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 // cấu hình bodyParser
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 // kết nối mongoose (database)
-const connectDB = require('./config/mongosee')
+const connectDB = require('./config/mongosee');
 connectDB();
 
-// import router 
-const production = require('./routers/product')
-app.use(production)
+// import router
+const production = require('./routers/product');
+app.use(production);
 
-app.get("/",function(req,res){
-    res.send("hello tuyên")
-})
+app.get('/', function (req, res) {
+  res.send('hello tuyên');
+});
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
